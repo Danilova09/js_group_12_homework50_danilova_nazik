238224c2223dae44ba2f4068adc270946b7c84cd
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2021-10-26 14:26:00 +0600

    Реализация компонента card-block

--- movie-app/src/app/app.module.ts
+++ movie-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { CardBlockComponent } from './card-block/card-block.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    CardBlockComponent
   ],
   imports: [
     BrowserModule
